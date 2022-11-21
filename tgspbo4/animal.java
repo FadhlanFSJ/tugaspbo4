b13278581e2081f14a0c56db400d6cbc8c258fd7
Fadhlan <96508486+FadhlanFSJ@users.noreply.github.com>
2022-11-21 21:01:15 +0700

    Update animal.java

--- tgspbo4/animal.java
+++ tgspbo4/animal.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tgspbo4;
 
 public class animal {
